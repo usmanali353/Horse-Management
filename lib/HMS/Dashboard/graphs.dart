0773f97e6a1b9098f7ff127074214bc0bbbc565c
Usman <aliusman353@gmail.com>
2020-03-30 11:45:00 -0700

    Added Income Expenses graphs

--- lib/HMS/Dashboard/graphs.dart
+++ lib/HMS/Dashboard/graphs.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:horse_management/HMS/Dashboard/dashboard_services/user_dashboard_json.dart';
 import 'package:horse_management/Model/breedingControlStatus.dart';
+import 'package:horse_management/Model/incomeExpenses.dart';
 import 'package:horse_management/Model/trainingStatus.dart';
 import 'package:horse_management/Utils.dart';
 import 'package:pie_chart/pie_chart.dart';
@@ -22,9 +23,10 @@ class graphsState extends State<graphs>{
   String token;
   var dashboard_data;
   graphsState(this.token);
-  List<charts.Series> seriesList,seriesList2;
+  List<charts.Series> seriesList,seriesList2,seriesLineList;
   List<charts.Series<trainingStatus, String>> trainingStatusData;
   List<charts.Series<breedingControlStatus,String>> breedingControlStatusData;
+  List<charts.Series<incomeExpenses,int>> incomeExpensesData;
   Map<String, double> dataMap;
   bool isVisible=false;
   List<Color> colorList = [
@@ -82,8 +84,58 @@ class graphsState extends State<graphs>{
                   },
                 )
               ];
+              List<incomeExpenses> incomeList=[
+                incomeExpenses(1,double.parse(dashboard_data['incomes'][0].toString())),
+                incomeExpenses(2,double.parse(dashboard_data['incomes'][1].toString())),
+                incomeExpenses(3,double.parse(dashboard_data['incomes'][2].toString())),
+                incomeExpenses(4,double.parse(dashboard_data['incomes'][3].toString())),
+                incomeExpenses(5,double.parse(dashboard_data['incomes'][4].toString())),
+                incomeExpenses(6,double.parse(dashboard_data['incomes'][5].toString())),
+                incomeExpenses(7,double.parse(dashboard_data['incomes'][6].toString())),
+                incomeExpenses(8,double.parse(dashboard_data['incomes'][7].toString())),
+                incomeExpenses(9,double.parse(dashboard_data['incomes'][8].toString())),
+                incomeExpenses(10,double.parse(dashboard_data['incomes'][9].toString())),
+                incomeExpenses(11,double.parse(dashboard_data['incomes'][10].toString())),
+                incomeExpenses(12,double.parse(dashboard_data['incomes'][11].toString())),
+              ];
+              List<incomeExpenses> expensesList=[
+                incomeExpenses(1,double.parse(dashboard_data['expenses'][0].toString())),
+                incomeExpenses(2,double.parse(dashboard_data['expenses'][1].toString())),
+                incomeExpenses(3,double.parse(dashboard_data['expenses'][2].toString())),
+                incomeExpenses(4,double.parse(dashboard_data['expenses'][3].toString())),
+                incomeExpenses(5,double.parse(dashboard_data['expenses'][4].toString())),
+                incomeExpenses(6,double.parse(dashboard_data['expenses'][5].toString())),
+                incomeExpenses(7,double.parse(dashboard_data['expenses'][6].toString())),
+                incomeExpenses(8,double.parse(dashboard_data['expenses'][7].toString())),
+                incomeExpenses(9,double.parse(dashboard_data['expenses'][8].toString())),
+                incomeExpenses(10,double.parse(dashboard_data['expenses'][9].toString())),
+                incomeExpenses(11,double.parse(dashboard_data['expenses'][10].toString())),
+                incomeExpenses(12,double.parse(dashboard_data['expenses'][11].toString())),
+              ];
+
+              incomeExpensesData=[
+                charts.Series<incomeExpenses,int>(
+                  domainFn: (incomeExpenses status, _) => status.month,
+                  measureFn: (incomeExpenses status, _) => status.value,
+                  data: incomeList,
+                  id: "Income",
+                  fillColorFn: (incomeExpenses status, _) {
+                    return charts.MaterialPalette.teal.shadeDefault;
+                  },
+                ),
+                charts.Series<incomeExpenses, int>(
+                  domainFn: (incomeExpenses status, _) => status.month,
+                  measureFn: (incomeExpenses status, _) => status.value,
+                  data: expensesList,
+                  id: "Expenses",
+                  fillColorFn: (incomeExpenses status, _) {
+                    return charts.MaterialPalette.teal.shadeDefault;
+                  },
+                )
+              ];
               seriesList=trainingStatusData;
               seriesList2=breedingControlStatusData;
+              seriesLineList=incomeExpensesData;
             });
           }
         });
@@ -92,7 +144,6 @@ class graphsState extends State<graphs>{
   }
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text("Dashboard"),),
       body: ListView(
@@ -159,19 +210,19 @@ class graphsState extends State<graphs>{
                                  renderSpec:  charts.SmallTickRendererSpec(
                                    // Tick and Label styling here.
                                      labelStyle:  charts.TextStyleSpec(// size in Pts.
-                                         color: charts.ColorUtil.fromDartColor(Colors.teal)),
+                                         color: charts.ColorUtil.fromDartColor(Colors.grey)),
 
                                      // Change the line colors to match text color.
                                      lineStyle: new charts.LineStyleSpec(
-                                         color: charts.ColorUtil.fromDartColor(Colors.teal)))),
+                                         color: charts.ColorUtil.fromDartColor(Colors.grey)))),
                                primaryMeasureAxis: new charts.NumericAxisSpec(
                                    renderSpec: new charts.GridlineRendererSpec(
                                      // Tick and Label styling here.
                                        labelStyle: new charts.TextStyleSpec(
-                                           color: charts.ColorUtil.fromDartColor(Colors.teal)),
+                                           color: charts.ColorUtil.fromDartColor(Colors.grey)),
                                        // Change the line colors to match text color.
                                        lineStyle: new charts.LineStyleSpec(
-                                           color: charts.ColorUtil.fromDartColor(Colors.teal)))),
+                                           color: charts.ColorUtil.fromDartColor(Colors.grey)))),
                            )
                          ),
 
@@ -198,24 +249,75 @@ class graphsState extends State<graphs>{
                             child: charts.BarChart(
                               seriesList2,
                               animate: true,
-                              vertical: true,
+                              vertical: false,
                               domainAxis:  charts.OrdinalAxisSpec(
                                   renderSpec:  charts.SmallTickRendererSpec(
                                     // Tick and Label styling here.
                                       labelStyle:  charts.TextStyleSpec(// size in Pts.
-                                          color: charts.ColorUtil.fromDartColor(Colors.teal)),
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)),
 
                                       // Change the line colors to match text color.
                                       lineStyle: new charts.LineStyleSpec(
-                                          color: charts.ColorUtil.fromDartColor(Colors.teal)))),
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)))),
                               primaryMeasureAxis: new charts.NumericAxisSpec(
                                   renderSpec: new charts.GridlineRendererSpec(
                                     // Tick and Label styling here.
                                       labelStyle: new charts.TextStyleSpec(
-                                          color: charts.ColorUtil.fromDartColor(Colors.teal)),
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)),
                                       // Change the line colors to match text color.
                                       lineStyle: new charts.LineStyleSpec(
-                                          color: charts.ColorUtil.fromDartColor(Colors.teal)))),
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)))),
+                            )
+                        ),
+
+                      ],
+
+                    ),
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: isVisible,
+                child: Container(
+                  margin: EdgeInsets.all(8),
+                  height:MediaQuery.of(context).size.height/2.7,
+                  child: Card(
+                    elevation:5.0,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top:8,bottom:8),
+                          child:Center(child: Text("Income and Expenses",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20))),
+                        ),
+                        Expanded(
+                            child: charts.LineChart(
+                              seriesLineList,
+                              animate: true,
+                                behaviors: [
+                                new charts.ChartTitle('Months',titleStyleSpec: charts.TextStyleSpec(color: charts.ColorUtil.fromDartColor(Colors.grey)),
+                                behaviorPosition: charts.BehaviorPosition.bottom,
+                                titleOutsideJustification:charts.OutsideJustification.middleDrawArea),
+                                  new charts.ChartTitle('Income Expenses',titleStyleSpec: charts.TextStyleSpec(color: charts.ColorUtil.fromDartColor(Colors.grey)),
+                                      behaviorPosition: charts.BehaviorPosition.start,
+                                      titleOutsideJustification: charts.OutsideJustification.middleDrawArea),
+                                ],
+                              domainAxis:  charts.NumericAxisSpec(
+                                  renderSpec:  charts.SmallTickRendererSpec(
+                                    // Tick and Label styling here.
+                                      labelStyle:  charts.TextStyleSpec(// size in Pts.
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)),
+
+                                      // Change the line colors to match text color.
+                                      lineStyle: new charts.LineStyleSpec(
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)))),
+                              primaryMeasureAxis: new charts.NumericAxisSpec(
+                                  renderSpec: new charts.GridlineRendererSpec(
+                                    // Tick and Label styling here.
+                                      labelStyle: new charts.TextStyleSpec(
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)),
+                                      // Change the line colors to match text color.
+                                      lineStyle: new charts.LineStyleSpec(
+                                          color: charts.ColorUtil.fromDartColor(Colors.grey)))),
                             )
                         ),
 
---
+++ lib/Model/incomeExpenses.dart
@@ -0,0 +1,6 @@
+class incomeExpenses{
+  int month;
+  double value;
+
+  incomeExpenses(this.month, this.value);
+}
