823604e5442928c63ac2e25f24e29093eacd157b
IIB <aliusman353@gmail.com>
2020-07-21 12:43:19 +0500

    Breeding Sales Fixed

--- lib/HMS/Breeding/BreedingSales/update_breeding_sales.dart
+++ lib/HMS/Breeding/BreedingSales/update_breeding_sales.dart
@@ -774,6 +774,7 @@ class _update_breeding_sales_form extends State<update_breeding_sales_form>{
                                 BreedingSalesServices.add_breeding_sales(specificsales['createdBy'],token,specificsales['breedingSalesId'],sale_response['horseDropDown'][selected_horse_id]['id'],DateTime.now(), sale_response['customerDropDown'][selected_customer_id]['id'],sale_response['assignedVetDropDown'][selected_vet_id]['id'],Payment_date,payment_reference.text, selected_semen_id,selected_frozen_id,selected_cashpaymemt_id,selected_gift_id,selected_status_id, contract_no.text, report_no.text, comments.text,amount.text, sale_response['currencyDropDown'][selected_currency_id]['id'], sale_response['categoryDropDown'][selected_category_id]['id'], sale_response['costCenterDropDown'][selected_costcenter_id]['id'], sale_response['contactsDropDown'][selected_contact_id]['id'],  ).then((response){
                                   pd.dismiss();
                                   if(response!=null){
+
                                     Scaffold.of(context).showSnackBar(SnackBar(
                                       content: Text("Breeding Sales Updated"),
                                       backgroundColor: Colors.green,
